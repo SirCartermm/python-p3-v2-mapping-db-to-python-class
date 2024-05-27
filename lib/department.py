21dcfc326243c97e05f10e6b569152ca7414fa13
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-05-27 16:49:58 +0300

    modified  the department.py

--- lib/department.py
+++ lib/department.py
@@ -1,74 +1,143 @@
+#lib/depertment.py
+
 from __init__ import CURSOR, CONN
 
 
-class Department:
+class Depertment:
+    
+    # Dictionary of object saved to the database.
+    all = {}
 
-    def __init__(self, name, location, id=None):
+    def __init__(self, name, location, id:None):
         self.id = id
         self.name = name
         self.location = location
+    
 
     def __repr__(self):
-        return f"<Department {self.id}: {self.name}, {self.location}>"
+        return f"<Department {self.id}: {self.id}: {self.name},{self.location}>"
+    
 
     @classmethod
     def create_table(cls):
-        """ Create a new table to persist the attributes of Department instances """
+        "'" Create a new table to persit the attributes of Department instance "'"
         sql = """
-            CREATE TABLE IF NOT EXISTS departments (
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            location TEXT)
-        """
+             CREATE TABLE IF NOT EXISTS departments (
+             id INTERGER PRIMARY KEY ,
+             name TEXT,
+             location TEXT)
+         """
         CURSOR.execute(sql)
         CONN.commit()
 
-    @classmethod
-    def drop_table(cls):
-        """ Drop the table that persists Department instances """
+        @classmethod
+        def drop_table(cls):
+            """ DROP the table tha persists Department instances """
+            sql = """
+                DROP TABLE IF EXISTS departments;
+            """
+            CURSOR.execute(sql)
+            CONN.commit()
+
+        def save(self):
+            """  Insert a new row with the name and location values of the current Department instance.
+            update object id attribute using the primary key value of the current Department instances.
+            save the object in local dictionary using table row's PK as dictionary key """
+            sql = """
+                  INSERT INTO departments (name, location)
+                  VALUES (?, ?)
+             """
+            
+            CURSOR.execute(sql, (self.name, self.location))
+            CONN.COMMIT()
+
+            self.id = CURSOR.lastrowid
+            type(self).all[self.all[self.id] = self
+                           
+        @classmethod
+        def create(cls, name, location):
+              """ Initialize a new Department instance and save the object to the database  """
+              department  # cls(name, location)
+              department.save()
+              return department 
+
+        def update(self):
+              """Update the table row corresponding to the current Department instance."""
+              sql = """
+                   UPDATE departments
+                   SET name = ?, location = ?
+                   WHERE id = ?
+            """
+            CURSOR.execute(sql, (self.name, self.location, self.id))
+            CONN.commit()
+
+        def delete(self):
+        """Delete the table row corresponding to the current Department instance,
+        delete the dictionary entry, and reasssign id attribute"""
+
+
         sql = """
-            DROP TABLE IF EXISTS departments;
-        """
-        CURSOR.execute(sql)
-        CONN.commit()
+DELETE FROM departments
+WHERE id = ?
+  """
 
-    def save(self):
-        """ Insert a new row with the name and location values of the current Department instance.
-        Update object id attribute using the primary key value of new row.
-        """
-        sql = """
-            INSERT INTO departments (name, location)
-            VALUES (?, ?)
-        """
+  # Delete the dictionary entry using id as the key
+  del type(self).all[self.id]
 
-        CURSOR.execute(sql, (self.name, self.location))
-        CONN.commit()
+  # Set the id to None
+  self.id = None
 
-        self.id = CURSOR.lastrowid
+@classmethod
+def instance_from_db(cls, row):
+ """Return a Department object having the attribute values from the table row."""
 
-    @classmethod
-    def create(cls, name, location):
-        """ Initialize a new Department instance and save the object to the database """
-        department = cls(name, location)
-        department.save()
-        return department
 
-    def update(self):
-        """Update the table row corresponding to the current Department instance."""
-        sql = """
-            UPDATE departments
-            SET name = ?, location = ?
-            WHERE id = ?
-        """
-        CURSOR.execute(sql, (self.name, self.location, self.id))
-        CONN.commit()
+ # check th dictionary for an existing instance using the row's primary key
+  department = cls.all.get(row[0])
+  if department:
+  department.name = row[1]
+  department.location - row[2]
+else:
+    # not in dictionary, create new instance and add to dictionary 
+    department = cls(row[1, row[2]])
+    department.id = row[0]
+    cls.all[department.id] = department
+return department
 
-    def delete(self):
-        """Delete the table row corresponding to the current Department instance"""
-        sql = """
-            DELETE FROM departments
-            WHERE id = ?
-        """
+@classmethod
+def get_all(cls):
+ """Return a list containing a Department object per row in the table"""
+ sql = """
+ SELECT *
+ FROM departments
+ 
+ """
 
-        CURSOR.execute(sql, (self.id,))
-        CONN.commit()
+ rows = CURSOR.execute(sql).fetchall()
+
+ return [cls.instance_from_db(row) for row in rows]
+
+
+@classmethod
+def find_by_id(cls, id):
+     """Return a Department object corresponding to the table row matching the specified primary key"""
+     sql = """
+          SELECT *
+          FROM departments
+          WHERE id = ?
+        """ CURSOR.execute(sql, (id,)).fetchone()
+        return cls.instance_from_db(row) if row else None
+
+
+@classmethod
+def find_by_name(cls, name):
+"""Return a Department object corresponding to first table row matching specified name """
+sql = """
+    SELECT *
+    FROM departments
+    WHERE name is ?
+"""
+
+row = CURSOR.execute(sql, (name,)).fetchone()
+return cls.instance_from_db(row) if row else None
+
